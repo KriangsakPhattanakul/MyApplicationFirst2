66782f2790b7f59db96022ed0d25331ab5ee8f11
Kriangsak <ketclup.277@gmail.com>
2017-07-27 17:00:29 +0700

    Initial commit

---
+++ app/src/main/java/suwanan/myapplicationfirst/MainActivity.java
@@ -0,0 +1,29 @@
+package suwanan.myapplicationfirst;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE ="com.example.myfirstapp.MESSAGE" ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void onsendClicked (View View){
+        Intent intent = new Intent(this, page2.class);
+        EditText editText = (EditText) findViewById(R.id.input);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+
+    }
+
+}
